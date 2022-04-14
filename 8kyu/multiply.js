10567d8b047a7bb708f55d4ded4073a32b5d547a
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-04-13 23:09:59 -0400

    multiply Codewars

---
+++ 8kyu/multiply.js
@@ -0,0 +1,11 @@
+//This code does not execute properly. Try to figure out why.
+//CODE WITH ERRORS
+
+// function multiply(a, b){
+//   a * b
+// }
+
+//CODE FIXED
+function multiply(a, b){
+  return  a * b
+}
