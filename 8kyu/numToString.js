b8b621fcc4695d2a1e17ba77cf2e125d70687a73
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-03 15:02:46 -0400

    Convert String to Number

---
+++ 8kyu/numToString.js
@@ -0,0 +1,12 @@
+/*
+We need a function that can transform a number into a string.
+
+Examples:
+123 --> "123"
+999 --> "999"
+*/
+
+//Here the method toString() is taking in the parameter num which would be input by the user and converting it to a string.
+function numberToString(num) {
+return num.toString();
+}
