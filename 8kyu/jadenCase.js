f64d2adda3df5a066fbe1089dd75c84e50243824
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-04 10:28:32 -0400

    Jaden Casing Strings

---
+++ 8kyu/jadenCase.js
@@ -0,0 +1,11 @@
+/*String.prototype.toJadenCase = function () {
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}*/
+
+String.prototype.toJadenCase = function () {
+  return this.split(" ").map(function(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(" ");
+}
