f409ee8f660ccd675387f92f271359d5e299c87b
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-07 16:43:37 -0400

    rock paper scissors codewars

---
+++ 8kyu/rock-paper-scissors.js
@@ -0,0 +1,29 @@
+// //Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+
+const rps = (p1, p2) => {
+     const sci = "scissors"
+     const paper = "paper"
+     const rock = "rock"
+
+       if (p1 === rock && p2 === paper) {
+       return "Player 2 won!"
+     } else if (p1 === sci && p2 === paper) {
+       return "Player 1 won!"
+     } else if (p1 === paper && p2 === rock) {
+       return "Player 1 won!"
+     } else if (p1 === rock && p2 === sci){
+       return "Player 1 won!"
+     } else if (p2 === rock && p1 === paper) {
+       return "Player 1 won!"
+     } else if (p2 === sci && p1 === paper) {
+       return "Player 2 won!"
+     } else if (p2 === paper && p1 === rock) {
+       return "Player 2 won!"
+     } else if (p2 === rock && p1 === sci){
+       return "Player 2 won!"
+     } else {
+       return "Draw!"
+     }
+};
