20790d9980a72daf4eeb2ecca876ff2a46e8a5e9
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-16 14:35:42 -0400

    Define a card suit

--- 8kyu/defineCardSuit.js
+++ 8kyu/defineCardSuit.js
@@ -1,13 +1,13 @@
 /*You get any card as an argument. Your task is to return the suit of this card (in lowercase).*/
 
 function defineSuit(card) {
-if (card[1] === '♣' ){
+if (card[card.length-1] === '♣' ){
   return 'clubs'
-}else if (card[1] === '♦') {
+}else if  (card[card.length-1]=== '♦') {
   return 'diamonds'
-} else if (card[1] === '♥') {
+} else if (card[card.length-1] === '♥') {
   return 'hearts'
-} else if (card[1] === '♠') {
+} else if (card[card.length-1] === '♠') {
   return 'spades'
 } else {
   "invalid card type"
