00e2e4071043779139dc4859582e7eeba7edc91e
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-05-20 23:34:19 -0400

    Hello Happy Codevarrior

---
+++ 8kyu/helloHappyCodevarrior.js
@@ -0,0 +1,19 @@
+/*VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+*/
+class Warrior {
+  constructor(newName) {
+    this.wName = newName
+  }
+
+  name(newName) {
+    if (newName) {
+      this.wName = newName
+    }
+    return this.wName
+  }
+}
