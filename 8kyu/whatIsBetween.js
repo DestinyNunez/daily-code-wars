187f00783e42469495ea497a9fc7d7319d2eb78f
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-07-19 15:26:51 -0400

    update-What is between

--- 8kyu/whatIsBetween.js
+++ 8kyu/whatIsBetween.js
@@ -7,3 +7,17 @@ function between(a, b) {
   }
   return array;
 }
+
+
+/*Write a function that will take in 2 intgers a, b and return only the even numbers between a and b NOT INCLUDING a, b. a will allways be less than b */
+
+function evenNumsBetween(a, b) {
+  let array = [];
+    for (let i = a+1; i<=b-1; i++) {
+      if(i % 2 == 0) {
+        array.push(i)
+    }
+  }
+    return array;
+}
+console.log(numLine(0, 50))
