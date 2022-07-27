f1d90a601ee877bd53ad9d50e8d8a2946708172e
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-07-27 19:13:33 -0400

    print

--- 7kyu/vowelCount.js
+++ 7kyu/vowelCount.js
@@ -9,7 +9,7 @@ function getCount(str) {
   for (let i = 0; i< str.length; i++ ) {
     if (str[i] === "a" || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u')  {
      vowelsCount += 1
-    }
+    } 
   }
 
   return vowelsCount;
