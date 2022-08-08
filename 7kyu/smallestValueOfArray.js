3abfa9d9aec1840128d5c3ea038b171e9422cd9f
DestinyNunez <88361309+DestinyNunez@users.noreply.github.com>
2022-08-07 23:46:54 -0400

    new codewar

---
+++ 7kyu/smallestValueOfArray.js
@@ -0,0 +1,13 @@
+// pseudocode
+// 1. Create a function called smallestValue
+// 2. Determine if checking for value or index
+// using an if statement
+// 3. Create a variable to store the min value/or index
+// 4. Once I know what my second parameter is, i will then
+// provide the appropriate function
+// if its by index, im going to loop through the array
+// and check if array at index at i is less than index i + 1,
+// then store in index that variable using the array indexOf
+function min(arr, toReturn) {
+  // TODO
+}
